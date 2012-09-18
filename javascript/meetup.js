2dc04e35e65ae78bba9899325c89559c0432d0de
Gilbert Hartmann <BertHartm@gmail.com>
2012-09-17 23:37:13 -0400

    make the event processing code trigger asynchronously, fix object references

--- javascript/meetup.js
+++ javascript/meetup.js
@@ -1,11 +1,31 @@
+var events = [];
+
+function renderCal() {
+  var text = "";
+
+  $.each(events, function(index, val) {
+    var dteNow = new Date(val.time);
+    text += '<p><a href="'+ val.event_url + '">' + val.name + '</a> ' + dteNow.toLocaleString() + '</p>';
+  });
+
+  $('#calendar').html(text);
+}
+
+function orderEvents(ev1, ev2) {
+  if (ev1.time < ev2.time) {
+    return -1;
+  } else if (ev1.time == ev2.time) {
+    return 0;
+  } else {
+    return 1;
+  }
+}
 
 $(document).ready(function() {
-  var events = [];
-  var text='';
   var currDate = new Date();
   var currTime = currDate.getTime();
   var monthAhead = currTime+2592000000;
-  var tech_groups = ['MakerBar','NJTechBreakfast'];
+  var tech_groups = ['MakerBar','NJTechBreakfast','NJTechMixer','njtech'];
 
   $.each(tech_groups,function(index, group) {
     var URL = 'https://api.meetup.com/2/events.json/?group_urlname='+group+'&sign=true&time='+currTime+','+monthAhead+'&status=upcoming&key=676e1e5f20b623b131e2e4a553b7b41';
@@ -16,19 +36,16 @@ $(document).ready(function() {
       timeout: 10000,
       success: function(data, status) {
         $.each(data.results,function(index,val){
-          events.push([val.name,val.event_url,val.time]);
+          events.push(val);
         });
+
+        events.sort(orderEvents);
+
+        renderCal();
       },
-      error: function() {
-        alert("Error: __");
+      error: function(xhr, status, error) {
+        alert("Error: " + status);
       }
     });
   });
-  $.each(events, function(index, val) {
-    var dteNow = new Date(val.time);
-    text += '<p><a href="'+ val.event_url + '">' + val.name + '</a> ' + dteNow.toLocaleString() + '</p>';
-  });
-
-
-  $('#calendar').html(text);
 });
